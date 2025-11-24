6534a8c91ef2b4dbab565e00989b3c4e15a8b36e
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2025-11-24 07:00:16 +0100

    solved a day in the lift

---
+++ challanges/a_day_in_the_lift.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def solve():
+    floors = {
+        i: tuple(map(int, row.split(" ")))
+        for i, row in enumerate(
+            Path("./input.txt").read_text().strip().splitlines()
+        )
+    }
+    count = 1
+    floor = 0
+    direction = 1
+    while floor in floors:
+        stay, magnitude = floors[floor]
+        if not stay:
+            direction *= -1
+        floor += magnitude * direction
+        count += 1
+    print(count)
---
+++ main.py
@@ -0,0 +1,9 @@
+from challanges import a_day_in_the_lift
+
+
+def main():
+    a_day_in_the_lift.solve()
+
+
+if __name__ == "__main__":
+    main()
